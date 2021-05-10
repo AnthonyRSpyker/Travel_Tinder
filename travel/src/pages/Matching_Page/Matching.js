1a290de98f192ce9def454bb2798154021bb7cc3
Anthony R. Spyker <anthonyspyker@gmail.com>
2021-05-10 19:41:10 -0400

    small addition to the pages. trieing to make a matching page.

---
+++ travel/src/pages/Matching_Page/Matching.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Navbar from "../../components/Navbar/Navbar"
+import ProfileCard from "../../compnents/ProfileCard/ProfileCard"
+
+function Matching () {
+
+    return(
+        <div>
+        <Navbar />
+            <ProfileCard />
+        
+        </div>
+
+
+
+    )
+}
+
+export default Matching;
