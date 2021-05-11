b20f053381d581d46de27e426bfb9bfeca06e1fc
joshua Korf <joshkorf08@gmail.com>
2021-05-11 17:04:57 -0400

    sign up page almost complete

--- travel/client/src/pages/signup/signup.js
+++ travel/client/src/pages/signup/signup.js
@@ -22,32 +22,41 @@ function Signup() {
               <span class="input-group-label">
                 <i class="fa fa-user"></i>
               </span>
-              <input class="input-group-field" type="text" placeholder="Full name"></input>
+              <input class="input-group-field" type="text" placeholder="Full name" value=""></input>
             </div>
 
             <div class="input-group">
               <span class="input-group-label">
                 <i class="fa fa-envelope"></i>
               </span>
-              <input class="input-group-field" type="text" placeholder="Email"></input>
+              <input class="input-group-field" type="text" placeholder="Email" value=""></input>
             </div>
 
             <div class="input-group">
               <span class="input-group-label">
                 <i class="fa fa-key"></i>
               </span>
-              <input class="input-group-field" type="text" placeholder="Password"></input>
+              <input class="input-group-field" type="text" placeholder="Password" value=""></input>
+            </div>
+            <div class="input-group">
+              <span class="input-group-label">
+                <i class="fa fa-key"></i>
+              </span>
+              <input class="input-group-field" type="text" placeholder="Confirm Password" value=""></input>
+            </div>
+            <div class="input-group">
+              <span class="input-group-label">
+                <i class="fa fa-key"></i>
+              </span>
+              <input class="input-group-field" type="text" placeholder="Age" value=""></input>
+            </div>
+            <div class="input-group">
+              <span class="input-group-label">
+                <i class="fa fa-key"></i>
+              </span>
+              <input class="input-group-field" type="text" placeholder="Prefered Gender" value=""></input>
             </div>
           </div>
-
-         
-
-
-
-
-
-
-
         </div>
 
 
@@ -62,77 +71,170 @@ function Signup() {
   </h5>
             <div class="polls-options">
               <div>
-                <input id="checkbox1" type="checkbox"></input>
-                <label for="checkbox1">Spanish</label>
+                <input id="checkbox1-a" type="checkbox"></input>
+                <label class="checkbox1-a">Spanish</label>
               </div>
               <div>
                 <input id="checkbox2" type="checkbox"></input>
-                <label for="checkbox2">Manderine</label>
+                <label class="checkbox2">Manderine</label>
               </div>
               <div>
                 <input id="checkbox3" type="checkbox"></input>
-                <label for="checkbox3">French</label>
+                <label class="checkbox3">French</label>
               </div>
               <div>
                 <input id="checkbox4" type="checkbox"></input>
-                <label for="checkbox4">German</label>
+                <label class="checkbox4">German</label>
               </div>
             </div>
-            <div class="polls-submit">
-              <input type="submit" class="button" value="Submit Vote"></input>
-            </div>
+            
           </div>
           <h5 class="polls-question">
-              <span class="polls-question-label">Q:</span>
+            <span class="polls-question-label">Q:</span>
     What Continents do you want to travel to?
   </h5>
-  <div class="polls-options">
-              <div>
-                <input id="checkbox1" type="checkbox"></input>
-                <label for="checkbox1">Spanish</label>
-              </div>
-              <div>
-                <input id="checkbox2" type="checkbox"></input>
-                <label for="checkbox2">Manderine</label>
-              </div>
-              <div>
-                <input id="checkbox3" type="checkbox"></input>
-                <label for="checkbox3">French</label>
-              </div>
-              <div>
-                <input id="checkbox4" type="checkbox"></input>
-                <label for="checkbox4">German</label>
-              </div>
+          <div class="polls-options">
+
+            <div>
+              <input id="checkbox1" type="checkbox"></input>
+              <label class="checkbox1">Asian</label>
             </div>
-            <div class="polls-submit">
-              <input type="submit" class="button" value="Submit Vote"></input>
+            <div>
+              <input id="checkbox2" type="checkbox"></input>
+              <label class="checkbox2">Bangkok, Thailand</label>
+            </div>
+            <div>
+              <input id="checkbox2" type="checkbox"></input>
+              <label class="checkbox2">Singapore, Singapore</label>
             </div>
 
-          <h1>What is your prefered Gender?</h1>
 
-          <div class="mobile-app-toggle" data-mobile-app-toggle>
-            <button class="button">Male</button>
-            <button class="button">Female</button>
-            <button class="button">Other</button>
+            <div>
+              <input id="checkbox1" type="checkbox"></input>
+              <label class="checkbox1">Africa</label>
+            </div>
+            <div>
+              <input id="checkbox1" type="checkbox"></input>
+              <label class="checkbox2">South, Africa</label>
+            </div>
+            <div>
+              <input id="checkbox2" type="checkbox"></input>
+              <label class="checkbox2">Tunisia</label>
+            </div>
+
+
+            <div>
+              <input id="checkbox1" type="checkbox"></input>
+              <label class="checkbox1">Australia/ Oceania</label>
+            </div>
+            <div>
+              <input id="checkbox2" type="checkbox"></input>
+              <label class="checkbox2">Sydney</label>
+            </div>
+            <div>
+              <input id="checkbox2" type="checkbox"></input>
+              <label class="checkbox2">Cocos (keeling) Islands</label>
+            </div>
+
+
+            <div>
+              <input id="checkbox1" type="checkbox"></input>
+              <label class="checkbox1">Europe</label>
+            </div>
+            <div>
+              <input id="checkbox2" type="checkbox"></input>
+              <label class="checkbox2">Italy</label>
+            </div>
+            <div>
+              <input id="checkbox2" type="checkbox"></input>
+              <label class="checkbox2">Greece</label>
+            </div>
+
+            <div>
+              <input id="checkbox1" type="checkbox"></input>
+              <label class="checkbox1">North America</label>
+            </div>
           </div>
+          <div>
+            <input id="checkbox2" type="checkbox"></input>
+            <label class="checkbox2">New York City, New York</label>
+          </div>
+          <div>
+            <input id="checkbox2" type="checkbox"></input>
+            <label class="checkbox2">Miami, Florida</label>
+          </div>
+          <div>
+            <input id="checkbox1" type="checkbox"></input>
+            <label class="checkbox1">South America</label>
+          </div><div>
+            <input id="checkbox2" type="checkbox"></input>
+            <label class="checkbox2">Bogota, Colombia</label>
+          </div><div>
+            <input id="checkbox2" type="checkbox"></input>
+            <label class="checkbox2">Santiago de Chile, Chile</label>
+          </div>
+        </div>
 
+
+        
+        <div class="status-update-input-box">
+          <form>
+            <div class="row input-box-container">
+              <div class="columns">
+                <label> Personal Bio Seciton (500 characters max.)
+          <textarea rows="5" placeholder="Hey Harry, What's on your mind?"></textarea>
+                </label>
+              </div>
+            </div>
+            {/* <div class="row medium-collapse user-action-container">
+              <div class="small-12 medium-7 columns">
+                <div class="user-action-box">
+                  <div class="action-area">
+                    <a href="#" class="action-anchor has-tip bottom" data-tooltip aria-haspopup="true" data-disable-hover="false" tabindex="2" title="Upload a Photo">
+                      <i class="fa fa-picture-o" aria-hidden="true"></i>
+                      <span class="show-for-sr">Upload a Photo</span>
+                    </a>
+                  </div>
+                  <div class="action-area">
+                    <a href="#" class="action-anchor has-tip bottom" data-tooltip aria-haspopup="true" data-disable-hover="false" tabindex="2" title="Upload a Video">
+                      <i class="fa fa-video-camera" aria-hidden="true"></i>
+                      <span class="show-for-sr">Upload a Video</span>
+                    </a>
+                  </div>
+                  <div class="action-area">
+                    <a href="#" class="action-anchor has-tip bottom" data-tooltip aria-haspopup="true" data-disable-hover="false" tabindex="2" title="Check in">
+                      <i class="fa fa-map-marker" aria-hidden="true"></i>
+                      <span class="show-for-sr">Check in</span>
+                    </a>
+                  </div>
+                  <div class="action-area">
+                    <a href="#" class="action-anchor has-tip bottom" data-tooltip aria-haspopup="true" data-disable-hover="false" tabindex="2" title="Add a Feeling">
+                      <i class="fa fa-smile-o" aria-hidden="true"></i>
+                      <span class="show-for-sr">Add a Feeling</span>
+                    </a>
+                  </div>
+                  <div class="action-area">
+                    <a href="#" class="action-anchor has-tip bottom" data-tooltip aria-haspopup="true" data-disable-hover="false" tabindex="2" title="Tag a Friend">
+                      <i class="fa fa-tags" aria-hidden="true"></i>
+                      <span class="show-for-sr">Tag a Friend</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="small-12 medium-5 columns">
+                <div class="user-submit-box">
+                  <input type="submit" class="button small" value="POST"></input>
+                </div>
+              </div>
+            </div> */}
+          </form>
         </div>
 
 
 
-
-      
-       
-  
-  
-
-  <button class="button expanded" onClick = {() => history.push("../matching_page")}>Sign Up</button>
-</form>
-</div>
-    )};
-
-
-
-
-
+        <button class="button expanded" onClick={() => history.push("../matching_page")}>Sign Up</button>
+      </form>
+    </div>
+  )
+};
 export default Signup;
