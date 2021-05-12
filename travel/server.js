264d41ef55f1ec055648d81b3200f1e1b4218db5
Ult Des <truttling.mariah@gmail.com>
2021-05-12 17:55:31 -0400

    added states and context to signin page. Added Service and COntext folder housing script for singin page auth. Install needed node modules

--- travel/server.js
+++ travel/server.js
@@ -1,45 +1,45 @@
-const express = require("express");
+// const express = require("express");
 
-const mongoose = require("mongoose");
-const routes = require("./routes");
-const app = express();
-const PORT = process.env.PORT || 3001;
+// const mongoose = require("mongoose");
+// const routes = require("./routes");
+// const app = express();
+// const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  }
-
-app.use(routes);
-
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/"
-  );
-
-app.listen(PORT, function() {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
-
-
-// const express= require('express');
-// const  app= express();
-// const cookieParser= require('cookie-parser');
-// const mongoose= require('mongoose')
-// app.use(cookieParser());
+// app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
 
-// const PORT = process.env.PORT || 3000;
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/<dbname>", {
-//   useNewUrlParser: true,
-  
-//    useUnifiedTopology: true 
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/build"));
+//   }
+
+// app.use(routes);
+
+// mongoose.connect(
+//     process.env.MONGODB_URI || "mongodb://localhost/"
+//   );
+
+// app.listen(PORT, function() {
+//     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 // });
 
-// const userRouter= require('./routes/User');
-// app.use('/user', userRouter);
 
-// app.listen(PORT,()=>{
-//     console.log('express sever started')
-// }); 
+ const express= require('express');
+ const  app= express();
+ const cookieParser= require('cookie-parser');
+ const mongoose= require('mongoose')
+ app.use(cookieParser());
+ app.use(express.json());
+
+ const PORT = process.env.PORT || 3002;
+ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/travelt", {
+   useNewUrlParser: true,
+  
+    useUnifiedTopology: true 
+ });
+
+ const userRouter= require('./routes/User');
+ app.use('/user', userRouter);
+
+ app.listen(PORT,()=>{
+     console.log('express sever started')
+ }); 
