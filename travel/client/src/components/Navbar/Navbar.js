61bbd386cba701f42edf65a44b391e166d62c4ed
joshua Korf <joshkorf08@gmail.com>
2021-05-12 14:11:22 -0400

    nav bar almost complete

--- travel/client/src/components/Navbar/Navbar.js
+++ travel/client/src/components/Navbar/Navbar.js
@@ -7,22 +7,27 @@ function NavBar() {
   const history = useHistory()
     return (
   
-
+<div class="container-row-1">
 <div class="social-links">
-  <div class="row">
+  <div class="row-1">
     <h1>TravelTinder</h1>
-    <div class="small-6 medium-3 columns text-center mobile-stack">
-      <a onClick = {() => history.push("../")}><i class="fa fa-facebook" aria-hidden="true"></i>Log out</a>
+    <div class="small-6 medium-3 columns  mobile-stack">
+      <p onClick = {() => history.push("../")}><i class="fa fa-facebook" aria-hidden="true"></i>Log out</p>
     </div>
-    <div class="small-6 medium-3 columns text-center mobile-stack">
-      <a onClick = {() => history.push("../matches")}><i class="fa fa-instagram" aria-hidden="true"></i>Matches</a>
     </div>
-    <div class="small-6 medium-3 columns text-center mobile-stack">
-      <a onClick = {() => history.push("../profile")}><i class="fa fa-pinterest-p" aria-hidden="true"></i>Profile</a>
+    <div class="row-1">
+    <div class="small-6 medium-3 columns mobile-stack">
+      <p onClick = {() => history.push("../matches")}><i class="fa fa-instagram" aria-hidden="true"></i>Matches</p>
+    </div>
+    </div>
+    <div class="row-1">
+    <div class="small-6 medium-3 columns mobile-stack">
+      <p onClick = {() => history.push("../profile")}><i class="fa fa-pinterest-p" aria-hidden="true"></i>Profile</p>
+    </div>
     </div>
   </div>
-</div>
 
+</div>
 
        
     )
